4ee470b5c023eabd6b919f5f3979280f86b0cb7b
Paulo Victor Mourão <mouraopaulovictor@gmail.com>
2024-11-02 22:30:33 -0400

    vai tomando

--- user_models.py
+++ user_models.py
@@ -1,10 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-
-db = SQLAlchemy()
+from config import *
 
 
 class User(db.Model):
+    __tablename__ = 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.Text, nullable=False)
